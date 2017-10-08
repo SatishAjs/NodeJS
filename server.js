5e626dedbdfa0f5b83099bc635c5bbee24212ea5
SatishAjs <32596937+SatishAjs@users.noreply.github.com>
2017-10-08 21:18:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http= require("http");
 
 var server = http.createServer(function(req,res){
    res.writeHead(200,{"content-type":"text/plain"});
-   res.end("hello Satish")
+   res.end("hello Satish from Node")
 });
 
 server.listen(7000);
